a40843deebf66a47fa752c1c1753eb5bf0be1b5a
xxyzz <gitpull@protonmail.com>
2026-09-10 13:04:50 +0800

    Change MathJax SVG size unit from `ex` to `px`
    and remove `a` element's `background: none;` CSS rule, it makes the link a back block in
    MuPDF 1.28.3

--- src/wiktionary_stardict/mathjax.ts
+++ src/wiktionary_stardict/mathjax.ts
@@ -5,13 +5,44 @@ await MathJax.init({
   },
   svg: {
     fontCache: "none",
+    postFilters: [
+      ({data}) => {
+        // MuPDF default font size for SVG is too small:
+        // https://bugs.ghostscript.com/show_bug.cgi?id=709705
+        const adaptor = MathJax.startup.adaptor;
+        const outputJax = MathJax.startup.document.outputJax;
+
+        const svg = adaptor.tags(data, "svg")[0];
+        if (svg) {
+          const pxPerEm = outputJax.pxPerEm;
+          const fixed = outputJax.fixed;
+
+          const viewBox = adaptor.getAttribute(svg, "viewBox");
+          if (viewBox) {
+            const [, , w, h] = viewBox.trim().split(/\s+/).map(Number);
+
+            adaptor.setAttribute(svg, "width", `${fixed(w * pxPerEm / 1000)}px`);
+            adaptor.setAttribute(svg, "height", `${fixed(h * pxPerEm / 1000)}px`);
+          }
+
+          const style = adaptor.getAttribute(svg, "style");
+          if (style) {
+            const match = style.match(/(-?\d+(?:\.\d+)?)ex/);
+            if (match) {
+              const value = fixed(Number(match[1]) * pxPerEm / 1000);
+              adaptor.setAttribute(svg, "style", `vertical-align: ${value}px`);
+            }
+          }
+        }
+      }
+    ]
   },
 });
 
 async function tex2svg(input: string): Promise<string> {
   const node = await MathJax.tex2svgPromise(
     input,
-    { display: true, em: 20, ex: 10 },
+    { display: true, em: 36, ex: 18 },
   );
   return MathJax.startup.adaptor.serializeXML(node);
 }
